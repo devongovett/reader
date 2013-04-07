6b6fb550321c13384662a1bb49802c11a4d3d2cb
Devon Govett <devongovett@gmail.com>
2013-04-06 23:09:47 -0700

    Stubs for preference APIs

---
+++ src/api/preference.js
@@ -0,0 +1,21 @@
+var express = require('express'),
+    db = require('../db'),
+    utils = require('../utils');
+    
+var app = module.exports = express();
+
+app.get('/reader/api/0/preference/list', function(req, res) {
+    
+});
+
+app.get('/reader/api/0/preference/set', function(req, res) {
+    
+});
+
+app.get('/reader/api/0/preference/stream/list', function(req, res) {
+    
+});
+
+app.get('/reader/api/0/preference/stream/set', function(req, res) {
+    
+});
